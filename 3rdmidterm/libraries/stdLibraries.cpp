0ede7a8ca768cfc7d9581c35904dede99137657d
Bruno Sánchez <90653850+BrunoSaPa@users.noreply.github.com>
2025-05-13 19:39:22 -0600

    libraries

---
+++ 3rdmidterm/libraries/stdLibraries.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string b="alo";
+    string c="bye";
+
+    cout << "SWAP\n";
+    b.swap(c);
+    cout<<"b: " << b << endl;
+}
