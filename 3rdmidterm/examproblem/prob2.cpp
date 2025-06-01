fe8b1959ad37d98510b1c4389efdbd205297c362
Bruno Sánchez <90653850+BrunoSaPa@users.noreply.github.com>
2025-06-01 16:37:31 -0600

    Update prob2.cpp

--- 3rdmidterm/examproblem/prob2.cpp
+++ 3rdmidterm/examproblem/prob2.cpp
@@ -1,74 +1,144 @@
+#include <array>
+#include <climits>
 #include <iostream>
-#include <string>
 #include <vector>
+#include <unordered_map>
 #include <algorithm>
 
 using namespace std;
+int amount_of_changes(const array<int, 9> &ns, const array<int, 3> ans) {
+    int sum = 0;
+    for (size_t i = 0; i < 3; i++) {
+        for (size_t j = 0; j < 3; j++) {
+            if (j == ans[i])
+            continue;
+            
+            sum += ns[i*3+j];
+        }
+    }
+    return sum;
+}
+
+bool solve() {
+    const array<array<int, 3>, 6> possible_answers{
+        {
+            {0, 2, 1}, 
+            {0, 1, 2}, 
+            {2, 1, 0},
+            {2, 0, 1}, 
+            {1, 0, 2},
+            {1, 2, 0}, 
+         }};
+    unordered_map<int, string> names{
+        {0, "AMBAR"},
+        {1, "VERDE"},
+        {2, "TRANSPARENTE"},
+    };
+    array<int, 9> ns;
+    vector<int> correct_ans;
+    for (auto &n : ns) {
+        cin >> n;
+        if (n==0 && cin.eof()) return false;
+    }
+    int min_i=-1;
+    int mn = INT_MAX;
+    for ( int i=0; i<6;i++) {
+        int sum = amount_of_changes(ns, possible_answers[i]);
+        if (sum < mn) {
+            mn = sum;
+            min_i = i;
+        }
+        mn = min(sum, mn);
+    }
+    correct_ans.push_back(min_i);
+
+    for (int i = 0; i<6;i++) {
+        if (i == min_i) continue;
+        if (amount_of_changes(ns, possible_answers[min_i]) == amount_of_changes(ns, possible_answers[i]))
+        {
+            correct_ans.push_back(i);
+        }
+    }
+    auto correct_it = min(correct_ans.begin(), correct_ans.end());
+    for (auto el: possible_answers[*correct_it])
+        cout << names[el] <<' ' ;
+
+    cout << '\n';
+    return true;
+}
 
 int main() {
-    int a1, a2, a3, v1, v2, v3, t1, t2, t3;
-
-    while (true) {
-        cin >> a1 >> a2 >> a3 >> v1 >> v2 >> v3 >> t1 >> t2 >> t3;
-        if (a1 == 0) break;
-
-        vector<int> bottles = {a1, a2, a3, v1, v2, v3, t1, t2, t3};
-       
-        vector<vector<int>> configs = {
-            {0, 1, 2}, // AVT
-            {0, 2, 1}, // ATV
-            {1, 0, 2}, // VAT
-            {1, 2, 0}, // VTA
-            {2, 0, 1}, // TAV
-            {2, 1, 0}  // TVA
-        };
-        vector<string> tags = {"AMBAR", "VERDE", "TRANSPARENTE"};
-        
-        int minMoves = 10000000;
-        vector<int> bestConfig;
-        
-        for(auto& x : configs) {
-    
-            int moves = 0;
-           
-            for(int i = 0; i < 3; i++) {
-                int type = x[i]; 
-                
-                for(int j = 0; j < 3; j++) {
-                    if(j != type) {
-                        moves += bottles[i * 3 + j];
-                    }
-                }
-            }
-            
-            
-            if (moves < minMoves) {
-                minMoves = moves;
-                bestConfig = x;
-            } else if (moves == minMoves) {
-              
-                vector<string> actual = {
-                    tags[x[0]],
-                    tags[x[1]],
-                    tags[x[2]]
-                };
-                vector<string> mejor = {
-                    tags[bestConfig[0]],
-                    tags[bestConfig[1]],
-                    tags[bestConfig[2]]
-                };
-                if (actual < mejor) {
-                    bestConfig = x;
-                }
-            }
-        }
-        
-        vector<string> posToTag(3);
-        for (int pos = 0; pos < 3; pos++) {
-            posToTag[pos] = tags[bestConfig[pos]];
-        }
-        cout << posToTag[0] << " " << posToTag[1] << " " << posToTag[2] << endl;
+    while(solve()) {
     }
-    
-    return 0;
+}include <array>
+#include <climits>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+int amount_of_changes(const array<int, 9> &ns, const array<int, 3> ans) {
+    int sum = 0;
+    for (size_t i = 0; i < 3; i++) {
+        for (size_t j = 0; j < 3; j++) {
+            if (j == ans[i])
+            continue;
+            
+            sum += ns[i*3+j];
+        }
+    }
+    return sum;
+}
+
+bool solve() {
+    const array<array<int, 3>, 6> possible_answers{
+        {
+            {0, 2, 1}, 
+            {0, 1, 2}, 
+            {1, 0, 2},
+            {1, 2, 0}, 
+            {2, 0, 1}, 
+            {2, 1, 0}
+         }};
+    unordered_map<int, string> names{
+        {0, "AMBAR"},
+        {1, "VERDE"},
+        {2, "TRANSPARENTE"},
+    };
+    array<int, 9> ns;
+    vector<int> correct_ans;
+    for (auto &n : ns) {
+        cin >> n;
+        if (n==0) return false;
+    }
+    int min_i=-1;
+    int mn = INT_MAX;
+    for ( int i=0; i<6;i++) {
+        int sum = amount_of_changes(ns, possible_answers[i]);
+        if (sum < mn) {
+            mn = sum;
+            min_i = i;
+        }
+        mn = min(sum, mn);
+    }
+    correct_ans.push_back(min_i);
+
+    for (int i = 0; i<6;i++) {
+        if (i == min_i) continue;
+        if (amount_of_changes(ns, possible_answers[min_i]) == amount_of_changes(ns, possible_answers[i]))
+        {
+            correct_ans.push_back(i);
+        }
+    }
+
+    for (auto id: possible_answers[*min(correct_ans.begin(), correct_ans.end())])
+        cout << names[id] << ' ';
+
+    cout << '\n';
+    return true;
+}
+
+int main() {
+    while(solve()) {
+    }
 }
